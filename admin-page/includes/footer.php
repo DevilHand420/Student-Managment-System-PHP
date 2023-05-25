be44f63029ad231ee34958f5f699b89a27506d26
Dikshant <104151198+DevilHand420@users.noreply.github.com>
2023-05-25 12:24:55 +0530

    Add files via upload

---
+++ admin-page/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer d-flex justify-content-center align-items-center">
+  <div class="d-flex flex-column justify-content-center align-items-center">
+    <img src="images/21.png" alt="Peacemaker University logo" style="width: 50px; height: auto; margin-bottom: 20px;">
+   <div class="copyright-left">
+      <p>© <?php echo date('Y');?> , Peacemaker University </p>
+      </div>
+  </div>
+</footer>
