850bab93108fd9e8538bf5431162d578e55192af
Dikshant <104151198+DevilHand420@users.noreply.github.com>
2023-05-25 12:27:05 +0530

    Add files via upload

---
+++ student-page/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer d-flex justify-content-center align-items-center">
+  <div class="d-flex flex-column justify-content-center align-items-center">
+    <img src="images/peace.png" alt="Peacemaker University logo" style="width: 50px; height: auto; margin-bottom: 10px;">
+    <span class="text-muted text-center" style="color: blue; font-size: 1.2em;">Peacemaker University</span>
+  </div>
+</footer>
